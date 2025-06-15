30d233bcc59b956c9943ad1a370169696a0074ff
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-06-15 04:34:16 -0300

    fix(Application): Rename Application Service Exception class

---
+++ CalculoImposto.Api/Application/Exceptions/ApplicationServiceException.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculoImposto.Api.Application.Exceptions
+{
+    public class  ApplicationServiceException : Exception
+    {
+        public ApplicationServiceException(string message, Exception? domainException = null)
+            : base(message, domainException)
+        {
+        }
+    }
+}
