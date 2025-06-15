367f29ad32fc2308bb3096ed0d74b542e55aaba9
Allan Cezar Almeida Chaves <allancac@gmail.com>
2025-06-15 04:24:03 -0300

    feat(Application): Criar classe de exceção para aplicação
    - Criar ApplicationException

---
+++ CalculoImposto.Api/Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculoImposto.Api.Application.Exceptions
+{
+    public class  ApplicationException : Exception
+    {
+        public ApplicationException(string message, Exception? domainException = null)
+            : base(message, domainException)
+        {
+        }
+    }
+}
